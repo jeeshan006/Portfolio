6e2d6ddb51f03780f2312d788673b4a3caaa3205
Mohd Jeeshan <jeeshanmohammad921@gmail.com>
2025-08-26 20:42:01 +0530

    Add Resumeon my hero section in resume button

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Hero.css'
 import profile_image from '../../assets/DP.png';
+import {Link} from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+
 
 const Hero = () => {
   return (
@@ -9,8 +12,18 @@ const Hero = () => {
       <h1><span>I'm Mohd Jeeshan</span><br /> Web Developer  </h1>
       <p>A passionate Web Developer with skills in HTML, CSS, JavaScript, Core Java,React,Tailwind,Bootstrap and MySQL.</p>
       <div className="hero-action">
-        <div className="hero-connect">Connect with me</div>
-        <div className="hero-resume">My Resume</div>
+        <div className="hero-connect" onClick={() => {
+      window.open(
+        "www.instagram.com/jeeshan006",
+        "_blank"
+      );
+    }}>Connect with me</div>
+        <div className="hero-resume" onClick={() => {
+      window.open(
+        "https://drive.google.com/file/d/1osF3aC-5vjsLpQf_Y5vHM8AAPU4tVIcj/view?usp=drive_link",
+        "_blank"
+      );
+    }}>My Resume</div>
       </div>
     </div>
   )
